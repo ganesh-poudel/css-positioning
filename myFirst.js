594cf10c2a5ec165aae8471bb0b21eadbc5bcbf7
ganeshrubina <35303753+ganeshrubina@users.noreply.github.com>
2019-10-31 18:48:07 +0200

    Add files via upload

---
+++ myFirst.js
@@ -0,0 +1,66 @@
+/*var car = {
+	colo : "red",
+	speed : 20,
+	drive : function(){return "drive" ;}
+};
+
+var shoppingList = [
+	
+	"orange",
+	"mango",
+	20
+
+]*/
+
+/*var house = {
+
+	color : "Red",
+	floor : 2,
+	adress : "kontula",
+	room : ["kitchen", {bedroom : 3, toilet: "attached", include: ["walls", "doors", "vantalations"]}],
+	drive: function(){return "drive";},
+	stop : function(){}
+};*/
+
+/*var array =[
+
+	"string",
+	200,
+	{car: "volvo", model : "v60"},
+	["embeded", 300],
+	function() {return drive;},
+
+
+]*/
+/*
+function name(fullname){
+	
+
+	return fullname.firstname + fullname.lastname;
+}
+
+console.log(name({firstname: "Ganesh", lastname: "poudel"}));
+
+function addaress(fulladdress){
+
+	return fulladdress();
+}
+
+console.log(addaress(function(){ return "hello"}));*/
+
+/*function Apple(color, weight){
+
+	this.color = color;
+	this.weight = weight;
+	
+};
+
+Apple.prototype = {
+	eat : function(){return "eat the apple";},
+	throw : function(){return "throw the apple";}
+};
+
+var apple1 = new Apple("red", 200);
+var apple2 = new Apple("Green", 150);
+var apple3 = new Apple("Yellow", 100);*/
+
